682cc4d5ceef5189e05f680ffe28c5d20a5dda42
Fizal Sarif <fizal.sarif@gmail.com>
2020-04-04 16:04:51 -0400

    base built

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'development',
   entry: './frontend/index.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'public/js'),
     filename: 'main.bundle.js'
   }
 };
